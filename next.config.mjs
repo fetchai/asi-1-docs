c650e6b661630189f9e298ff0f2eba8e69934e0d
Felix Nicolae Bucsa <felix.bucsa@fetch.ai>
2025-05-15 12:13:42 +0100

    fix(docs): redirects

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,61 @@ export default withNextra({
         destination: "/docs",
         permanent: true,
       },
+      {
+        source: "/examples/ai-engine-api/chat_api_example",
+        destination: "/docs",
+        permanent: true
+      },
+      {
+        source: "/concepts/ai-engine/ai-engine-intro",
+        destination: "/docs",
+        permanent: true
+      },
+      {
+        source: "/concepts/ai-engine/deltav",
+        destination: "/docs",
+        permanent: true
+      },
+      {
+        source: "/concepts/ai-engine/powering-connections-and-smart-operations-in-deltav",
+        destination: "/docs",
+        permanent: true
+      },
+      {
+        source: "/concepts/ai-engine/ai-engine-personalities",
+        destination: "/docs",
+        permanent: true
+      },
+      {
+        source: "/guides/ai-engine-sdk/javascript",
+        destination: "/docs",
+        permanent: true
+      },
+      {
+        source: "/guides/ai-engine-sdk/python",
+        destination: "/docs",
+        permanent: true
+      },
+      {
+        source: "",
+        destination: "/docs",
+        permanent: true
+      },
+      {
+        source: "",
+        destination: "/docs",
+        permanent: true
+      },
+      {
+        source: "",
+        destination: "/docs",
+        permanent: true
+      },
+      {
+        source: "",
+        destination: "/docs",
+        permanent: true
+      }
     ]
   }
 })
